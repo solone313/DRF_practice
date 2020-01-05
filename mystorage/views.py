71db0a0293eaec21c08fb0d8b4e525ca407687e1
solone313 <solone313@naver.com>
2020-01-05 15:47:12 +0900

    get_queryset present user

--- mystorage/views.py
+++ mystorage/views.py
@@ -11,3 +11,15 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+    # 현재 request를 보낸 유저
+    # == self.request.user
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        
+        if self.request.user.is_authenticated:
+            qs = qs.filter(author = self.request.user)
+        else:
+            qs = qs.none()
+        return qs
