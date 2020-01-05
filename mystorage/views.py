01ac541b4c33403f91575bb462ebc23c28e5d499
solone313 <solone313@naver.com>
2020-01-05 15:50:03 +0900

    add searchFilter

--- mystorage/views.py
+++ mystorage/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Essay
 from .serializers import EssaySerializer
+from rest_framework.filters import SearchFilter
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Essay.objects.all()
     serializer_class = EssaySerializer
 
-
+    filter_backends = [SearchFilter]
+    search_fields = ('title','body')
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
